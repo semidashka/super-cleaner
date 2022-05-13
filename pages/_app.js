fe739500cd071d1fd4f4bf179eb734cf8b81bba2
semidashka <87187700+semidashka@users.noreply.github.com>
2022-05-13 14:43:07 +0200

    Initial commit

---
+++ pages/_app.js
@@ -0,0 +1,28 @@
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyle></GlobalStyle>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default MyApp;
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
+
+const theme = {
+  colors: {
+    primary: "#0070f3",
+  },
+};
